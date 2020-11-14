fac3aacbc51979becfe253aca1d731706fa31655
Jesús Gándara Martínez <jesusalxen@gmail.com>
2020-11-14 17:33:17 +0100

    feat: First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = express();
+const port = 3003;
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello wordl!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
